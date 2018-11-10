9ec399d12c85d94baa2410bcbf3b962883edd10b
EyihMohamedVall <mohamed-vall.eyih@devinci.fr>
2018-11-10 16:37:49 +0100

    Update user registration controller

--- app/User.php
+++ app/User.php
@@ -16,7 +16,7 @@ class User extends Authenticatable
      * @var array
      */
     protected $fillable = [
-        'name', 'email', 'password',
+        'nickname', 'firstname', 'lastname', 'dateOfBirth', 'game', 'email', 'password',
     ];
 
     /**
