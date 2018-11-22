2ae956bd2adffadad68137d086bcb201c11992fc
Baptiste BRIOIS <baptiste.briois@hotmail.fr>
2018-11-22 17:06:54 +0100

    Add Model and Migration for Role

---
+++ app/Models/Role.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Role extends Model
+{
+    use SoftDeletes;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'role';
+
+    /**
+     * The attributes that should be mutated to dates.
+     *
+     * @var array
+     */
+    protected $dates = ['deleted_at'];
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'type_id',
+        'game_id',
+        'label'
+    ];
+
+    /////////////////////
+    /// Relationships ///
+    /////////////////////
+
+    /**
+     * Get type related to the role
+     *
+     * @access public
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function type()
+    {
+        return $this->belongsTo('App\Models\Type');
+    }
+
+    /**
+     * Get game related to the role
+     *
+     * @access public
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsTo
+     */
+    public function game()
+    {
+        return $this->belongsTo('App\Models\Game');
+    }
+}
