e9e7dca4b9a811ec854b4f8a13a645de210875d2
Baptiste BRIOIS <baptiste.briois@hotmail.fr>
2018-11-25 17:49:29 +0100

    Add Model and Migration for Game

---
+++ app/Models/Game.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Game extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'game';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name'
+    ];
+
+    /////////////////////
+    /// Relationships ///
+    /////////////////////
+
+    /**
+     * Get roles related to the game
+     *
+     * @access public
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function roles()
+    {
+        return $this->hasMany('App\Models\Role');
+    }
+
+    /**
+     * Get game levels related to the game
+     *
+     * @access public
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\HasMany
+     */
+    public function gameLevels()
+    {
+        return $this->hasMany('App\Models\GameLevel');
+    }
+}
