a4ec858114c20dc6d68c0dc5146d6bd0b4ef4096
EyihMohamedVall <mohamed-vall.eyih@devinci.fr>
2019-02-17 18:48:58 +0100

    Create controller function for user dashboard

--- app/Http/Controllers/GameController.php
+++ app/Http/Controllers/GameController.php
@@ -1,8 +1,13 @@
 <?php
 
 namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Auth;
 
+use App\Models\Game;
+use App\Models\User;
+use App\Models\GameLevel;
 use Illuminate\Http\Request;
+use MaddHatter\LaravelFullcalendar\Calendar;
 
 class GameController extends Controller
 {
@@ -13,7 +18,69 @@ class GameController extends Controller
      */
     public function index()
     {
-        //
+        $events = [];
+
+        $events[] = \Calendar::event(
+            'Event One', //event title
+            false, //full day event?
+            '2015-02-11T0800', //start time (you can also use Carbon instead of DateTime)
+            '2015-02-12T0800', //end time (you can also use Carbon instead of DateTime)
+            0 //optionally, you can specify an event ID
+        );
+
+        $events[] = \Calendar::event(
+            "Valentine's Day", //event title
+            true, //full day event?
+            new \DateTime('2015-02-14'), //start time (you can also use Carbon instead of DateTime)
+            new \DateTime('2015-02-14'), //end time (you can also use Carbon instead of DateTime)
+            'stringEventId' //optionally, you can specify an event ID
+        );
+
+        $calendar = \Calendar::addEvents($events) //add an array with addEvents
+       ->setOptions([ //set fullcalendar options
+            'firstDay' => 1
+        ])->setCallbacks([ //set fullcalendar callback options (will not be JSON encoded)
+            'viewRender' => 'function() {alert("Callbacks!");}'
+        ]);
+        $user = Auth::user();
+        $games = Game::all();
+        $userGames = $user->games()->get();
+        return view('user-interface.dashboard', compact('user', 'games', 'userGames', 'calendar'));
+    }
+
+    public function fetchGameLevel(Request $request) {
+        $gameName = $request->get('value');
+        $game = Game::find($gameName);
+        $gameLevel = $game->gameLevels;
+        $output = '<option value="">Sélectionnez votre niveau de jeux</option>';
+        foreach($gameLevel as $row)
+        {
+            $output .= '<option value="'.$row->id.'">'.$row->label.'</option>';
+        }
+        echo $output;
+    }
+
+    public function storeGameUser(Request $request)
+    {
+        $validator = \Validator::make($request->all(), [
+            'game' => 'required',
+            'pseudo' => 'required',
+            'gameLevel' => 'required',
+        ]);
+
+        if ($validator->fails())
+        {
+            return response()->json(['errors'=>$validator->errors()->all()]);
+        }
+
+        $user_id = $user = Auth::user();
+        $game_level_id = $request->get('gameLevel');
+        $pseudo = $request->get('gameLevel');
+
+        $game = Game::find($request->get('game'));
+        $game->users()->attach($user_id, ['game_level_id' => $game_level_id, 'pseudo' => $pseudo]);
+
+        return response()->json(['success'=>'Data is successfully added']);
     }
 
     /**
@@ -36,6 +103,7 @@ class GameController extends Controller
     {
         //
     }
+
 
     /**
      * Display the specified resource.
